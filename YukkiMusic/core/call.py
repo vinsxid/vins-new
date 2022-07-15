e2d1ca7ad435c1dbe27e1de7d99e3e05e178ce48
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-07-16 04:58:52 +0700

    Update call.py

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -295,15 +295,15 @@ class Call(PyTgCalls):
                 )
             except Exception as e:
                 raise AssistantErr(
-                    "**No Active Voice Chat Found**\n\nPlease make sure group's voice chat is enabled. If already enabled, please end it and start fresh voice chat again and if the problem continues, try /restart"
+                    "**❌ **Obrolan Suara Tidak Ditemukan**\n\nPastikan obrolan suara grup diaktifkan. Jika sudah diaktifkan, harap akhiri dan mulai obrolan suara baru lagi, jika masih error coba /restart"
                 )
         except AlreadyJoinedError:
             raise AssistantErr(
-                "**Assistant Already in Voice Chat**\n\nSystems have detected that assistant is already there in the voice chat, this issue generally comes when you play 2 queries together.\n\nIf assistant is not present in voice chat, please end voice chat and start fresh voice chat again and if the  problem continues, try /restart"
+                "**Asisten Sudah di Obrolan Suara**\n\nSistem telah mendeteksi bahwa asisten sudah ada di obrolan suara, masalah ini biasanya muncul saat Anda memainkan 2 kueri secara bersamaan.\n\nJika asisten tidak ada di obrolan suara, harap akhiri obrolan suara dan mulai obrolan suara baru lagi, jika masih error coba /restart"
             )
         except TelegramServerError:
             raise AssistantErr(
-                "**Telegram Server Error**\n\nTelegram is having some internal server problems, Please try playing again.\n\n If this problem keeps coming everytime, please end your voice chat and start fresh voice chat again."
+                "**Telegram Server Error**\n\nTelegram mengalami beberapa masalah server internal. Silakan coba putar lagi.\n\nJika masalah ini terus muncul, harap akhiri obrolan suara Anda dan mulai obrolan suara baru lagi."
             )
         await add_active_chat(chat_id)
         await mute_off(chat_id)
