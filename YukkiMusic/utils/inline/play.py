6989653f0d85bee724a701b9405c3c4b9a9a3942
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-07-17 20:04:35 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -89,8 +89,9 @@ def stream_markup(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["PL_B_2"],
-                callback_data=f"add_playlist {videoid}",
+                #text=_["PL_B_2"],
+                #callback_data=f"add_playlist {videoid}",
+                text="📨 ᴄʜᴀɴɴᴇʟ", url=f"{config.SUPPORT_CHANNEL}"
             ),
             InlineKeyboardButton(
                 text=_["PL_B_3"],
@@ -247,15 +248,15 @@ def panel_markup_1(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="◀️",
+                text="«",
                 callback_data=f"Pages Back|0|{videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="🔙 Back",
+                text="🔙 Kembali",
                 callback_data=f"MainMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="▶️",
+                text="»",
                 callback_data=f"Pages Forw|0|{videoid}|{chat_id}",
             ),
         ],
@@ -285,15 +286,15 @@ def panel_markup_2(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="◀️",
+                text="«",
                 callback_data=f"Pages Back|1|{videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="🔙 Back",
+                text="🔙 Kembali",
                 callback_data=f"MainMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="▶️",
+                text="»",
                 callback_data=f"Pages Forw|1|{videoid}|{chat_id}",
             ),
         ],
@@ -325,15 +326,15 @@ def panel_markup_3(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="◀️",
+                text="«",
                 callback_data=f"Pages Back|2|{videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="🔙 Back",
+                text="🔙 Kembali",
                 callback_data=f"MainMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="▶️",
+                text="»",
                 callback_data=f"Pages Forw|2|{videoid}|{chat_id}",
             ),
         ],
